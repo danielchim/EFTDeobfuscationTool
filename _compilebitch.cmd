d21692f20a91428e87d225b5a3a20c99ca8cdf6c
PARGSoft <pargsoft@hotmail.com>
2022-05-12 08:57:00 +0100

    Updated to 0.12.12.15.17861

--- _compilebitch.cmd
+++ _compilebitch.cmd
@@ -1,3 +1,3 @@
-de4dot-x64.exe --un-name "!^<>[a-z0-9]$&!^<>[a-z0-9]__.*$&![A-Z][A-Z]\$<>.*$&^[a-zA-Z_<{$][a-zA-Z_0-9<>{}$.`-]*$" "Assembly-CSharp-cleaned.dll" --strtyp delegate --strtok "0x0600D724"
+de4dot-x64.exe --un-name "!^<>[a-z0-9]$&!^<>[a-z0-9]__.*$&![A-Z][A-Z]\$<>.*$&^[a-zA-Z_<{$][a-zA-Z_0-9<>{}$.`-]*$" "Assembly-CSharp-cleaned.dll" --strtyp delegate --strtok "0x0600D83C"
 
 pause
